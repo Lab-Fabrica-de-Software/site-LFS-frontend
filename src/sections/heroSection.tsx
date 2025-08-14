8b1853a894110cc6b32355a649241529a9e4a143
Yuri Alves <yuri.alves.silva@gmail.com>
2025-08-14 17:32:28 -0300

    :lipstick: fix: hero section breakpoints

--- src/sections/heroSection.tsx
+++ src/sections/heroSection.tsx
@@ -2,18 +2,18 @@ import { Button } from "@/components/ui/button";
 
 export function HeroSection() {
   return (
-    <div className="min-h-[85dvh] flex justify-center relative bg-[url('/hero-background.png')] bg-cover bg-center">
-      <div className="container flex flex-col items-center md:text-center py-24 z-10 relative">
-        <h1 className="text-4xl md:text-6xl text-white font-bold">
+    <div className="min-h-[80dvh] sm:min-h-[85dvh] flex justify-center relative bg-[url('/hero-background.png')] bg-cover bg-center">
+      <div className="container flex flex-col sm:items-center sm:text-center pt-10 sm:py-24 z-10 relative">
+        <h1 className="text-4xl max-[768px]:text-5xl md:text-6xl text-white font-bold">
           O primeiro passo para novos{" "}
-          <span className="block my-2">desenvolvedores se tornarem </span>
+          <span className="block sm:my-2">desenvolvedores se tornarem </span>
           <span className="text-primary block">profissionais</span>
         </h1>
-        <p className="text-foreground/80 roboto font-medium md:text-lg mt-5">
+        <p className="text-foreground/80 roboto font-medium sm:text-lg mt-5">
           Participe de projetos e acelere seu desenvolvimento.
           <span className="block">Construa sua experiência conosco!</span>
         </p>
-        <Button className="mt-5 text-lg px-6" aria-label="Estou interessado">
+        <Button className="mt-5 w-fit text-lg px-6" aria-label="Estou interessado">
           Estou interessado
         </Button>
       </div>
