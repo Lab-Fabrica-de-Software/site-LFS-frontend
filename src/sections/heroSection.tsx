252fa2fe28e87fdfac92191fd0a827a751a07a32
Yuri Alves <yuri.alves.silva@gmail.com>
2025-08-14 17:25:57 -0300

    :lipstick: feat: add image background and blur in hero section

--- src/sections/heroSection.tsx
+++ src/sections/heroSection.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export function HeroSection() {
   return (
-    <div className="min-h-[85dvh] flex justify-center relative">
+    <div className="min-h-[85dvh] flex justify-center relative bg-[url('/hero-background.png')] bg-cover bg-center">
       <div className="container flex flex-col items-center md:text-center py-24 z-10 relative">
         <h1 className="text-4xl md:text-6xl text-white font-bold">
           O primeiro passo para novos{" "}
@@ -18,7 +18,7 @@ export function HeroSection() {
         </Button>
       </div>
 
-      <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-[#A5DAAF]/20 via-transparent to-transparent backdrop-blur-3xl z-0"></div>
+      <div className="absolute bottom-0 left-0 right-0 h-100 bg-gradient-to-t from-[#A5DAAF]/20 via-transparent to-transparent backdrop-blur-3xl z-0"></div>
     </div>
   );
 }
