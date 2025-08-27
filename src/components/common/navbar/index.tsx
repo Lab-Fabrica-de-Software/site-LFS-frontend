f0a11b404f938c81a71a2be35acec84e4df26835
Yuri Alves <yuri.alves.silva@gmail.com>
2025-08-27 09:49:37 -0300

    :sparkles: feat: add hero section animations w/ motion react

--- src/components/common/navbar/index.tsx
+++ src/components/common/navbar/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "motion/react";
 import { Button, RedirectButton } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
@@ -19,19 +20,31 @@ export function Navbar({ socialIconsVisible = false, ...props }: NavbarProps) {
     const section = document.getElementById(id.replace("#", ""));
     if (section) {
       const isMobile = window.innerWidth <= 768;
-      section.scrollIntoView({ behavior: "smooth", block: isMobile ? "start" : "center" });
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: isMobile ? "start" : "center",
+      });
       setIsOpen(false);
     }
   };
-
 
   return (
     <header
       className="max-w-7xl z-50 fixed top-0 w-full left-0 right-0 lg:top-4 mx-auto lg:px-6"
       {...props}
     >
-      <nav className="border-b lg:border bg-card/95 md:bg-card/80 backdrop-blur-md lg:rounded-2xl py-3 px-4 lg:px-8 flex items-center justify-between">
-        <div className="flex items-center space-x-1">
+      <motion.nav
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="border-b lg:border bg-card/95 md:bg-card/80 backdrop-blur-md lg:rounded-2xl py-3 px-4 lg:px-8 flex items-center justify-between"
+      >
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.5 }}
+          className="flex items-center space-x-1"
+        >
           <Link href="/">
             <Image
               width={100}
@@ -42,12 +55,17 @@ export function Navbar({ socialIconsVisible = false, ...props }: NavbarProps) {
               priority
             />
           </Link>
-        </div>
+        </motion.div>
 
         <div className="hidden md:flex items-center space-x-4 text-white">
           <ul className="flex space-x-6">
             {navLinks.map((link, index) => (
-              <li key={index}>
+              <motion.li
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 + 0.5 * index }}
+                key={index}
+              >
                 {link.href[0] === "#" ? (
                   <button
                     onClick={() => scrollToSection(link.href)}
@@ -64,7 +82,7 @@ export function Navbar({ socialIconsVisible = false, ...props }: NavbarProps) {
                     {link.label}
                   </Link>
                 )}
-              </li>
+              </motion.li>
             ))}
           </ul>
 
@@ -103,11 +121,12 @@ export function Navbar({ socialIconsVisible = false, ...props }: NavbarProps) {
         >
           <Menu />
         </button>
-      </nav>
+      </motion.nav>
 
       <div
-        className={`fixed top-0 left-0 right-0 z-50 md:hidden transition-transform duration-500 ease-in-out border-b bg-card/95 backdrop-blur-md shadow-lg rounded-b-lg ${isOpen ? "translate-y-15" : "-translate-y-full"
-          }`}
+        className={`fixed top-0 left-0 right-0 z-50 md:hidden transition-transform duration-500 ease-in-out border-b bg-card/95 backdrop-blur-md shadow-lg rounded-b-lg ${
+          isOpen ? "translate-y-15" : "-translate-y-full"
+        }`}
       >
         <div className="p-4">
           <ul className="flex flex-col space-y-4 text-white text-lg">
@@ -135,7 +154,7 @@ export function Navbar({ socialIconsVisible = false, ...props }: NavbarProps) {
             <Link href="/">
               <Button className="py-1 h-8 text-sm">Ingresse Já</Button>
             </Link>
-            
+
             {!socialIconsVisible && (
               <div className="flex space-x-3 pt-2">
                 <Link
@@ -162,4 +181,3 @@ export function Navbar({ socialIconsVisible = false, ...props }: NavbarProps) {
     </header>
   );
 }
-
