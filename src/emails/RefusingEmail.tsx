630c7ff3a9813b454b8675c1e1862589703f459d
Guilherme <guilherme.narcizo@fatec.sp.gov.br>
2025-09-08 18:21:20 -0300

    :ok_hand: git hard made it and remaked

--- src/emails/RefusingEmail.tsx
+++ src/emails/RefusingEmail.tsx
@@ -1,19 +1,12 @@
-// /emails/ContactEmail.tsx
-import { ContactFormInputsProps } from "@/sections/contactFormSection";
-import { Html, Body, Heading, Text, Tailwind } from "@react-email/components";
+import { Html, Body, Text, Tailwind } from "@react-email/components";
 
-interface RefusingEmailProps {
-  data: RefusingEmailProps
-}
 
-export default function RefusingEmail({ data }: RefusingEmailProps) {
+
+export default function RefusingEmail() {
   return (
     <Html>
       <Tailwind>
         <Body className="font-sans py-2 px-4">
-          <Heading>
-
-          </Heading>
           <Text className="text-xs">Agradecemos muito por sua inscrição e pelo interesse em participar do <span className="text-[#56E873]">Laborátoria Fábrica de Software (LFS).</span></Text>
           <Text className="text-xs">Recebemos diversas candidaturas incríveis e, após uma análise criteriosa, infelizmente, neste momento, você não foi selecionado(a) para as próximas etapas do processo.</Text>
           <Text className="text-xs"><span className="text-[#56E873]">Essa decisão não diminui o valor do seu perfil nem o potencial que você demonstrou.</span> Reforçamos nosso agradecimento por ter dedicado seu tempo para se inscrever e compartilhar sua história conosco.</Text>
