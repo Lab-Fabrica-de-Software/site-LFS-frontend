4febccd9b37435c5c21eb5c4de30dfa7253259a2
Guilherme <guilherme.narcizo@fatec.sp.gov.br>
2025-09-24 11:30:59 -0300

    :construction: feat: projectContainer

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import { ProjectContainer } from "@/containers/projectContainer";
+
+export default function Page() {
+  return (
+    <main className="mt=2">
+      <ProjectContainer />
+    </main>
+  );
+}
