e5d0614d9d5929031771d4f553172a1667c5ae0c
Gttolla <lucas12chves@gmail.com>
2025-09-24 18:12:56 -0300

    :wrench: chore: GitHub webhook secret added to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,12 +7,15 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com'
       },
-      { 
+      {
         protocol: 'https',
         hostname: 'picsum.photos'
       }
     ]
-  }
+  },
+  env: {
+    GITHUB_WEBHOOK_SECRET: process.env.GITHUB_WEBHOOK_SECRET,
+  },
 };
 
 export default nextConfig;
