cf21205b510fa2a4b539982ccf8de254bf4b5f20
Gttolla <lucas12chves@gmail.com>
2025-10-02 16:45:56 -0300

    :bug: fix: projectsSection.stories.ts fixed

--- src/sections/projectsSection/projectsSection.stories.ts
+++ src/sections/projectsSection/projectsSection.stories.ts
@@ -16,56 +16,7 @@ type Story = StoryObj<typeof ProjectsSection>;
 
 export const Default: Story = {
   args: {
-    projects: [
-      {
-        id: '1',
-        title: 'Project One',
-        description: 'Description for project one.',
-        links: [
-          {
-            name: 'Teste',
-            url: 'Teste'
-          }
-        ],
-        status: "completed"
-      },
-      {
-        id: '2',
-        title: 'Project Two',
-        description: 'Description for project two.',
-        links: [
-          {
-            name: 'Teste',
-            url: 'Teste'
-          }
-        ],
-        status: "completed"
-      },
-      {
-        id: '3',
-        title: 'Project Three',
-        description: 'Description for project three.',
-        links: [
-          {
-            name: 'Teste',
-            url: 'Teste'
-          }
-        ],
-        status: "completed"
-      },
-      {
-        id: '4',
-        title: 'Project Four',
-        description: 'Description for project four.',
-        links: [
-          {
-            name: 'Teste',
-            url: 'Teste'
-          }
-        ],
-        status: "completed"
-      },
-    ],
+    showViewAllButton: true,
     limit: 3,
   },
 };
